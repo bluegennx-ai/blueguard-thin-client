6533d8f9669cee2d854dd18df817568658acfdab
Chakravarthy V P <chakravarthyvp@Chakravarthys-MacBook-Pro.local>
2024-07-09 09:28:22 +0530

    Initial Commit

---
+++ src/blueguard_client/components/__init__.py
@@ -0,0 +1,7 @@
+from .blueguard_requests import ClientGetRequests, ClientPostRequests
+from .blueguard_response import (
+    VersionResponse,
+    ProcessTextResponse,
+    ReIdentifyTextResponse
+)
+from .blueguard_uris import BlueguardURIs
